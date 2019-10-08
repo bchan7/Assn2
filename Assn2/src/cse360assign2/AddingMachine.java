5ead7e329bd163469288ae2f89f890f45d182672
Brandon Chan <bchan7@asu.edu>
2019-10-08 12:39:13 -0700

    Initial commit

---
+++ Assn2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,64 @@
+// Name: Brandon Chan
+// Assignment #: 2
+// Class ID: 70641
+// Description: This file does several mathematical operations such as
+//				adding and subtracting.
+
+package cse360assign2;
+
+public class AddingMachine 
+{
+
+	private int total;
+		
+	/**
+	 * 
+	 */
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+		
+	/**
+	 * 
+	 */
+	public String toString () 
+	{
+		return "";
+	}
+
+	/**
+	 * 
+	 */
+	public void clear() 
+	{
+	
+	}
+}
