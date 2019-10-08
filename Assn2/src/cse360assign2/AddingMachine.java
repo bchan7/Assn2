4e0f1f5681b8bb23ffa104ffe3e21d72adcfd8d9
Brandon Chan <bchan7@asu.edu>
2019-10-08 15:28:03 -0700

    Second/final version

--- Assn2/src/cse360assign2/AddingMachine.java
+++ Assn2/src/cse360assign2/AddingMachine.java
@@ -2,7 +2,7 @@
 // Assignment #: 2
 // Class ID: 70641
 // Description: This file does several mathematical operations such as
-//				adding and subtracting.
+//				adding and subtracting and also keeping track of the total.
 
 package cse360assign2;
 
@@ -10,40 +10,45 @@ public class AddingMachine
 {
 
 	private int total;
+	private String opString = "0";
 		
 	/**
-	 * 
+	 * Initializes total and opString
+	 * @param total will be the result after all mathematical operations
 	 */
+
 	public AddingMachine () 
 	{
 		total = 0;  // not needed - included for clarity
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Gives the current total
+	 * @return total, the current total after all operations
 	 */
 	public int getTotal () 
 	{
-		return 0;
+		return total;
 	}
 	
 	/**
-	 * 
-	 * @param value
+	 * Adds value to the total and then adds the addition operation to the opString
+	 * @param value, Will be added to the total
 	 */
 	public void add (int value) 
 	{
-		
+		total += value;
+		opString = opString + " + " + value;
 	}
 	
 	/**
-	 * 
-	 * @param value
+	 * Subtracts the value from the total and then adds the subtraction operation to the opString
+	 * @param value, Will be subtracted from the total
 	 */
 	public void subtract (int value) 
 	{
-		
+		total -= value;
+		opString = opString + " - " + value; 
 	}
 		
 	/**
@@ -51,14 +56,15 @@ public class AddingMachine
 	 */
 	public String toString () 
 	{
-		return "";
+		return opString;
 	}
 
 	/**
-	 * 
+	 * Sets the total and and opString back to 0
 	 */
 	public void clear() 
 	{
-	
+		total = 0;
+		opString = "0";
 	}
 }
